833120058ab5e12916b0b2f3bc02d9c8d1120446
pierrick <deshayes.pierrick@hotmail.fr>
2016-09-25 19:22:27 +0200

    update

--- src/alignement/BWT.java
+++ src/alignement/BWT.java
@@ -2,6 +2,7 @@ package alignement;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class BWT {
 	
@@ -13,34 +14,47 @@ public class BWT {
 		n = genome.length();
 	}
 
-	public char[] transform() {
+	public String transform() {
 		char[] res = new char[n];
 		String[] rotations = rotate();
 		Arrays.sort(rotations);
 		for (int i = 0; i < n; i++) {
 			res[i] = rotations[i].charAt(n-1);
 		}
-		return res;
+		return new String(res);
 	}
 	
-	public byte[] encode() {
-		byte[] res = new byte[n];
-		char[] bwt = transform();
-		ArrayList<Byte> alpha = new ArrayList<Byte>();
-		for (int i = 0; i < bwt.length; i++) {
-			res[i] = (byte) alpha.indexOf(bwt[i]);
-			alpha.remove(res[i]);
-			alpha.add(0, (byte) bwt[i]);
+	public int[] encode() {
+		int[] enc = new int[n];
+		String bwt = transform();
+		ArrayList<Character> alpha = alphabet();
+		for (int i = 0; i < bwt.length(); i++) {
+			int ind = alpha.indexOf(bwt.charAt(i));
+			enc[i] = ind;
+			alpha.remove(ind);
+			alpha.add(0, bwt.charAt(i));
 		}
+		return enc;
+	}
+	
+	public String decode() {
+		String res = new String();
+//		int[] sa = suffixArray();
+//		char[] t = transform();
+		
 		return res;
 	}
 	
-	private char[] alphabet() {
-		char[] alpha = new char[256];
+	public ArrayList<Character> alphabet() {
+		ArrayList<Character> alpha = new ArrayList<Character>();
 		for (int i = 0; i < genome.length(); i++) {
-//			genome.
+			char c = genome.charAt(i);
+			if (!alpha.contains(c)) {
+				alpha.add(c); 
+			}
 		}
-		return null;
+		Collections.sort(alpha);
+		return alpha;
 	}
 
 	private String[] rotate() {
@@ -51,6 +65,10 @@ public class BWT {
 		return rotations;
 	}
 
+	/**
+	 * 
+	 * @return un tableau d'entiers representant la table des suffixes
+	 */
 	public int[] suffixArray() {
 		int[] sa = new int[n];
 		String[] suffixes = new String[n];
@@ -64,12 +82,12 @@ public class BWT {
 		return sa;
 	}
 	
-	public void printTable(String[] tab) {
-		for (int i = 0; i < tab.length; i++) {
-			System.out.println(tab[i]);
-		}
-		System.out.println();
-	}
+//	public void printTable(String[] tab) {
+//		for (int i = 0; i < tab.length; i++) {
+//			System.out.println(tab[i]);
+//		}
+//		System.out.println();
+//	}
 	
 	public void printTable(int[] tab) {
 		for (int i = 0; i < tab.length; i++) {
@@ -78,11 +96,18 @@ public class BWT {
 		System.out.println();
 	}
 	
-	public void printTable(char[] tab) {
-		for (int i = 0; i < tab.length; i++) {
-			System.out.print(tab[i] + " ");
-		}
-		System.out.println();
-	}
+//	public void printTable(char[] tab) {
+//		for (int i = 0; i < tab.length; i++) {
+//			System.out.print(tab[i] + " ");
+//		}
+//		System.out.println();
+//	}
+//	
+//	public void printTable(byte[] tab) {
+//		for (int i = 0; i < tab.length; i++) {
+//			System.out.print(tab[i] + " ");
+//		}
+//		System.out.println();
+//	}
 
 }
--- src/alignement/TestBWT.java
+++ src/alignement/TestBWT.java
@@ -3,9 +3,12 @@ package alignement;
 public class TestBWT {
 
 	public static void main(String[] args) {
-		BWT bwt = new BWT("cgagacgaa$");
+		String genome = "que_chaque_chasseur_sachant_chasser_sache_chasser_sans_son_chien$";
+		BWT bwt = new BWT(genome);
+		System.out.println(genome);
 		bwt.printTable(bwt.suffixArray());
-		bwt.printTable(bwt.transform());
+		System.out.println(bwt.transform());
+		bwt.printTable(bwt.encode());
 
 	}
 
