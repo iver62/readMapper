73fe764d3a0002c9efa3d3a3ace23e20dc123826
pierrick <deshayes.pierrick@hotmail.fr>
2016-10-18 08:36:32 +0200

    ajout

---
+++ src/alignement/ScoresSet.java
@@ -0,0 +1,15 @@
+package alignement;
+
+public class ScoresSet {
+	
+	public int match;
+	public int sub;
+	public int gap;
+	
+	public ScoresSet(int match, int sub, int gap) {
+		this.match = match;
+		this.sub = sub;
+		this.gap = gap;
+	}	
+
+}
